2e2f542dce5f167e3d7286bae3767086b8ed088e
Ben O'Regan <benzy.oregan@gmail.com>
2026-03-25 17:43:43 +0000

    Fix Supabase client: use SyncClientOptions with http2=False

--- strava_pipeline/db/client.py
+++ strava_pipeline/db/client.py
@@ -6,7 +6,9 @@ Reads SUPABASE_URL and SUPABASE_SERVICE_KEY from the environment.
 """
 
 import os
+import httpx
 from supabase import create_client, Client
+from supabase.lib.client_options import SyncClientOptions
 
 _client: Client | None = None
 
@@ -16,5 +18,8 @@ def get_client() -> Client:
     if _client is None:
         url = os.environ["SUPABASE_URL"]
         key = os.environ["SUPABASE_SERVICE_KEY"]
-        _client = create_client(url, key)
+        _client = create_client(
+            url, key,
+            options=SyncClientOptions(httpx_client=httpx.Client(http2=False))
+        )
     return _client
